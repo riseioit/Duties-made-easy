8d2ec45b0345105b7cff4f5b45c43b3dfe0b4cef
vaibhav <vaibhavgoley3@gmail.com>
2020-07-14 23:28:38 +0530

    removed the extra commented code

--- main.js
+++ main.js
@@ -112,24 +112,3 @@ ls.on('close', (code) => {
 });
 
 });
-
-
-
-/*const fs = require('fs');
-const { spawn} = require("child_process")
-
-const ls = spawn('python', ['test.py']);
-
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
-
-ls.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
-});
-
-ls.on('close', (code) => {
-  console.log(`process exited with code ${code}`);
-});
-*/
-
